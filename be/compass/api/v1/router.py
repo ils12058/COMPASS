2c3fd1307b849ed82f5ea8235c23f587befab79a
ils12058 <ilssystems8@gmail.com>
2026-09-17 10:03:14 +0800

    Register Counseling API

--- be/compass/api/v1/router.py
+++ be/compass/api/v1/router.py
@@ -10,6 +10,7 @@ from compass.appointments.api import router as appointments_router
 from compass.authentication.api import router as authentication_router
 from compass.availability.api import router as availability_router
 from compass.common.errors import register_exception_handlers
+from compass.counseling.api import router as counseling_router
 from compass.organization.api import router as organization_router
 from compass.service_catalog.api import router as service_catalog_router
 
@@ -39,6 +40,10 @@ api = NinjaAPI(
                 "name": "appointments",
                 "description": "Shared Student and provider Appointment reservations.",
             },
+            {
+                "name": "counseling",
+                "description": "Assigned records of actual Counseling encounters.",
+            },
         ]
     },
     openapi_url="/openapi.json" if settings.API_DOCS_ENABLED else None,
@@ -52,4 +57,5 @@ api.add_router("/organization", organization_router)
 api.add_router("/services", service_catalog_router)
 api.add_router("/availability", availability_router)
 api.add_router("/appointments", appointments_router)
+api.add_router("/counseling", counseling_router)
 register_exception_handlers(api)
