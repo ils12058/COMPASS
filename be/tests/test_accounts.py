b42ddd97dc7cc8cd96a4f7a10ac1ac62d70c878b
ils12058 <ilssystems8@gmail.com>
2026-09-19 14:04:17 +0800

    Normalize Ruff test spacing in test_accounts.py

--- be/tests/test_accounts.py
+++ be/tests/test_accounts.py
@@ -452,7 +452,6 @@ def test_student_lifecycle_migration_backfills_only_existing_students():
     assert counselor.student_lifecycle_status is None
 
 
-
 @pytest.mark.django_db
 def test_institutional_officer_is_neutral_and_dpo_adds_no_capabilities():
     sync_policy()
