694bcf07c44055c66d8e0317d4c2249c95632e57
ils12058 <ilssystems8@gmail.com>
2026-09-20 10:03:30 +0800

    Make Service update provider input Counselor-only

--- be/compass/service_catalog/api.py
+++ be/compass/service_catalog/api.py
@@ -79,7 +79,7 @@ class ServiceUpdateRequest(StrictSchema):
     cancellation_cutoff_minutes: int | None = None
     requires_current_inventory: bool = False
     delivery_modes: list[DeliveryMode] = Field(default_factory=list)
-    provider_roles: list[ProviderRoleCode] = Field(default_factory=list)
+    provider_roles: list[ConfigurableProviderRoleCode] = Field(default_factory=list)
 
 
 class ServiceResponse(StrictSchema):
