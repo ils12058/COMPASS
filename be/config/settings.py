d0e7acd061eb12afb4cde4b456f9fe6e9f54007d
ils12058 <ilssystems8@gmail.com>
2026-09-20 21:05:40 +0800

    fix: support multipart PUT profile photos

--- be/config/settings.py
+++ be/config/settings.py
@@ -110,6 +110,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "ninja.compatibility.files.fix_request_files_middleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
