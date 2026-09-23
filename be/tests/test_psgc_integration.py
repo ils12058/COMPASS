0575dc27f2e46b785bf6c93b0e121be0a397c55e
ils12058 <ilssystems8@gmail.com>
2026-09-23 18:21:47 +0800

    style: match Ruff PSGC test formatting

--- be/tests/test_psgc_integration.py
+++ be/tests/test_psgc_integration.py
@@ -108,8 +108,7 @@ def test_psgc_client_uses_configured_version_token_and_parses_reference_levels(m
         ("0500000000", "Region V")
     ]
     assert [
-        (item.code, item.name)
-        for item in service.list_provinces(region_code="0500000000")
+        (item.code, item.name) for item in service.list_provinces(region_code="0500000000")
     ] == [("0501600000", "Camarines Norte")]
     assert [
         (item.code, item.name)
